6e010b97418566b419c14a6ea14c624d703e813f
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2023-03-20 21:38:40 -0300

    fluxo_dados

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,76 @@
-import express, { Request, Response } from 'express'
-import cors from 'cors'
-import { accounts } from './database'
-import { ACCOUNT_TYPE } from './types'
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { accounts } from "./database";
+import { ACCOUNT_TYPE } from "./types";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.listen(3003, () => {
-    console.log("Servidor rodando na porta 3003")
-})
+  console.log("Servidor rodando na porta 3003");
+});
 
 app.get("/ping", (req: Request, res: Response) => {
-    res.send("Pong!")
-})
+  res.send("Pong!");
+});
 
 app.get("/accounts", (req: Request, res: Response) => {
-    res.send(accounts)
-})
+  res.send(accounts);
+});
 
 app.get("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+  try {
+    
 
-    const result = accounts.find((account) => account.id === id) 
+    const id = req.params.id;
 
-    res.status(200).send(result)
-})
+    const result = accounts.find((account) => account.id === id);
+
+    if (!result) {
+      res.status(404);
+      throw new Error("Conta não encontrada. Verifique a ID");
+    }
+
+    res.status(200).send(result);
+  } catch (err) {
+    if(res.statusCode === 200){
+        res.status(500);
+    }
+    res.send(err.message);
+  }
+});
 
 app.delete("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+  const id = req.params.id;
 
-    const accountIndex = accounts.findIndex((account) => account.id === id)
+  const accountIndex = accounts.findIndex((account) => account.id === id);
 
-    if (accountIndex >= 0) {
-        accounts.splice(accountIndex, 1)
-    }
+  if (accountIndex >= 0) {
+    accounts.splice(accountIndex, 1);
+  }
 
-    res.status(200).send("Item deletado com sucesso")
-})
+  res.status(200).send("Item deletado com sucesso");
+});
 
 app.put("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+  const id = req.params.id;
 
-    const newId = req.body.id as string | undefined
-    const newOwnerName = req.body.ownerName as string | undefined
-    const newBalance = req.body.balance as number | undefined
-    const newType = req.body.type as ACCOUNT_TYPE | undefined
+  const newId = req.body.id as string | undefined;
+  const newOwnerName = req.body.ownerName as string | undefined;
+  const newBalance = req.body.balance as number | undefined;
+  const newType = req.body.type as ACCOUNT_TYPE | undefined;
 
-    const account = accounts.find((account) => account.id === id) 
+  const account = accounts.find((account) => account.id === id);
 
-    if (account) {
-        account.id = newId || account.id
-        account.ownerName = newOwnerName || account.ownerName
-        account.type = newType || account.type
+  if (account) {
+    account.id = newId || account.id;
+    account.ownerName = newOwnerName || account.ownerName;
+    account.type = newType || account.type;
 
-        account.balance = isNaN(newBalance) ? account.balance : newBalance
-    }
+    account.balance = isNaN(newBalance) ? account.balance : newBalance;
+  }
 
-    res.status(200).send("Atualização realizada com sucesso")
-})
+  res.status(200).send("Atualização realizada com sucesso");
+});
